f2ccdda5a9d27378e155a825aa4afbd3afd3167a
yeabTesfaye <tesfayeyeabsra446@gmail.com>
2024-10-02 12:25:43 +0300

    Displaying Platform Icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 
---
+++ src/components/PlatformIConList.tsx
@@ -0,0 +1,44 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+  FaLinux,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon } from "@chakra-ui/react";
+
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+interface Props {
+  platforms: Platform[];
+}
+const PlatformIConList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    andriod: FaAndroid,
+  };
+  return (
+    <HStack marginY="10px">
+      {platforms.map((platform) => (
+        <Icon
+          as={iconMap[platform.slug]}
+          key={platform.slug}
+          color="gray.500"
+        />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIConList;
