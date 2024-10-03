02fc7d474787fd42ec7ba9afdb65d17d6e5db79e
yeabTesfaye <tesfayeyeabsra446@gmail.com>
2024-10-03 08:12:57 +0300

    Displaying critic score

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 function NavBar() {
